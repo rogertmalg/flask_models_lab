6bf7007deaed983bb608a5f66e6dabf5a47e8bc5
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-03 15:12:04 +0000

    prints orders

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
+from models.orders import *
 
 @app.route('/orders')
 def orders():
-    return render_template("index.html", title="Orders")
+    # print(orders)
+    # print(orders.order1)
+    return render_template("index.html", title="Orders", orders=orders_list)
---
+++ models/orders.py
@@ -0,0 +1,6 @@
+from models.order import *
+
+order1 = Order("Charlie", "03/11/21", "1")
+order2 = Order("Ana", "01/11/21", "3")
+
+orders_list = [order1, order2]
