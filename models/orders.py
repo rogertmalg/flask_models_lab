4c88dd0e35550f949297c28ebe932f64718b1cb0
Douglas Russell <douglas.russell7@btinternet.com>
2021-11-03 15:16:27 +0000

    adds book_title attrivute to Order, updates template

--- models/orders.py
+++ models/orders.py
@@ -1,6 +1,6 @@
 from models.order import *
 
-order1 = Order("Charlie", "03/11/21", "1")
-order2 = Order("Ana", "01/11/21", "3")
+order1 = Order("Charlie", "03/11/21", "1", "Banana")
+order2 = Order("Ana", "01/11/21", "3", "Traffic")
 
 orders_list = [order1, order2]
