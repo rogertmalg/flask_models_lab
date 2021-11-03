82c460032ce527d6d6cd1d368982f88a424e99b7
rogertmalg <roger.malgueira@outlook.com>
2021-11-03 21:53:46 +0000

    add static directory and style.css folder. datetime imported to Class

--- models/orders.py
+++ models/orders.py
@@ -1,6 +1,7 @@
 from models.order import *
+import datetime
 
-order1 = Order("Charlie", "03/11/21", "1", "Banana")
-order2 = Order("Ana", "01/11/21", "3", "Traffic")
+order1 = Order("Charlie", datetime.date(2021, 11, 3), "1", "Banana")
+order2 = Order("Ana", datetime.date(2021, 11, 1), "3", "Traffic")
 
 orders_list = [order1, order2]
